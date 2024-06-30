5d20c3d89b8121b0aea0eee1b6f6e921913ae1cd
Geovanny Lara <jeffersonlara98@gmail.com>
2024-06-29 23:03:27 -0500

    csrf

--- app/config/settings.py
+++ app/config/settings.py
@@ -134,3 +134,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-5b9e.up.railway.app']
